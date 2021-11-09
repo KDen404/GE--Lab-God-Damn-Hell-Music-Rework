c0979f3606dfd4daf3098de3b2601b69e9446e0a
Stjpaa <87935031+Stjpaa@users.noreply.github.com>
2021-11-10 00:12:47 +0100

    Added character + few animations, added comments

--- God Damn Hell/Assets/Scripts/Player/PlayerMovement.cs
+++ God Damn Hell/Assets/Scripts/Player/PlayerMovement.cs
@@ -9,21 +9,17 @@ public class PlayerMovement : MonoBehaviour
 
     private Vector3 movement;
 
-    private void Start()
-    {
-        player = this.GetComponent<Rigidbody>();
-    }
-
     private void FixedUpdate()
     {
-        movement = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical")) * movementSpeed * Time.deltaTime;
-        player.MovePosition(transform.position + movement);
+        // Moves the player object with WADS
+        player.MovePosition(new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical")) * movementSpeed * Time.deltaTime + transform.position);
 
+        // Calculates the rotation using cursor position and player position
         Vector3 mousePosition = Input.mousePosition;
         Vector3 objectPosition = Camera.main.WorldToScreenPoint(transform.position);
         mousePosition.x = mousePosition.x - objectPosition.x;
         mousePosition.y = mousePosition.y - objectPosition.y;
         float angle = Mathf.Atan2(mousePosition.y, mousePosition.x) * Mathf.Rad2Deg;
-        player.MoveRotation(Quaternion.Euler(new Vector3(0, -angle, 0)));
+        player.MoveRotation(Quaternion.Euler(new Vector3(0, -angle + 90, 0))); // +90 necessary or wrong settings when exporting?
     }
 }
