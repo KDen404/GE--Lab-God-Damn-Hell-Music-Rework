66415c9ee07c9b641372defd0b02aa673c5bfad8
Stjpaa <87935031+Stjpaa@users.noreply.github.com>
2021-11-18 15:22:50 +0100

    Added layers (not complete yet)

--- God Damn Hell/Assets/Scripts/Player/PlayerAnimations.cs
+++ God Damn Hell/Assets/Scripts/Player/PlayerAnimations.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class PlayerAnimations : MonoBehaviour
 {
     private Animator animator;
+    public PlayerMovement playermovement;
+    private float angle;
+    private Vector2 movementAxis;
 
     // Start is called before the first frame update
     void Start()
@@ -14,20 +17,54 @@ public class PlayerAnimations : MonoBehaviour
 
     private void Update()
     {
+        Run();
+        Attack();
+
+    }
+
+    private void Run()
+    {
+        angle = playermovement.angle;
+        Debug.Log(angle);
+
+        movementAxis.x = Input.GetAxisRaw("Horizontal");
+        movementAxis.y = Input.GetAxisRaw("Vertical");
+
+        if (angle < 180 && angle > 90)
+        {
+            animator.SetFloat("MovementForward", movementAxis.y);
+            animator.SetFloat("MovementSides", movementAxis.x);
+        }
+
+        if (angle < 90 && angle > 0)
+        {
+            animator.SetFloat("MovementForward", movementAxis.y);
+            animator.SetFloat("MovementSides", movementAxis.x);
+        }
+
+        if (angle > -180 && angle < -90)
+        {
+            animator.SetFloat("MovementForward", -movementAxis.y);
+            animator.SetFloat("MovementSides", -movementAxis.x);
+        }
+
+        if (angle > -90 && angle < 0)
+        {
+            animator.SetFloat("MovementForward", -movementAxis.y);
+            animator.SetFloat("MovementSides", -movementAxis.x);
+        }
+    }
+
+    private void Attack()
+    {
         // Attacks if the player currently is in the idle state (else it queues and starts once arrived)
         if (Input.GetButtonDown("Fire1") && animator.GetCurrentAnimatorStateInfo(0).IsName("Armature_Idle_Sword_And_Shield"))
         {
             animator.SetTrigger("AttackTrigger");
         }
-
-        if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
-        {
-            animator.SetBool("isRunning", true);
-        }
-        else
-        {
-            animator.SetBool("isRunning", false);
-        }
     }
-
 }
+
+
+
+
