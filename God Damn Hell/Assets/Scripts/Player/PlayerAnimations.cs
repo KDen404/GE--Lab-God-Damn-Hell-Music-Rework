49973dd9fd45095ace47331c2709218c63206071
Stjpaa <87935031+Stjpaa@users.noreply.github.com>
2021-11-19 19:43:22 +0100

    Edited animations

--- God Damn Hell/Assets/Scripts/Player/PlayerAnimations.cs
+++ God Damn Hell/Assets/Scripts/Player/PlayerAnimations.cs
@@ -20,45 +20,97 @@ public class PlayerAnimations : MonoBehaviour
         Run();
         Attack();
 
+        Debug.Log("MovementForward: " + animator.GetFloat("MovementForward") + " and MovementSides: " + animator.GetFloat("MovementSides"));
     }
 
     private void Run()
     {
         angle = playermovement.angle;
-        Debug.Log(angle);
 
         movementAxis.x = Input.GetAxisRaw("Horizontal");
         movementAxis.y = Input.GetAxisRaw("Vertical");
 
-        if (angle < 180 && angle > 90)
+        if (angle <= 180 && angle > 90)
         {
-            animator.SetFloat("MovementForward", movementAxis.y);
-            animator.SetFloat("MovementSides", movementAxis.x);
+            if (angle <= 180 && angle > 150)
+            {
+                animator.SetFloat("MovementForward", -movementAxis.x);
+                animator.SetFloat("MovementSides", movementAxis.y);
+            }
+            else if (angle < 150 && angle > 120)
+            {
+                animator.SetFloat("MovementForward", movementAxis.y);
+                animator.SetFloat("MovementSides", movementAxis.x);
+            }
+            else if (angle < 120 && angle >= 90)
+            {
+                animator.SetFloat("MovementForward", movementAxis.y);
+                animator.SetFloat("MovementSides", movementAxis.x);
+            }
         }
 
-        if (angle < 90 && angle > 0)
+        if (angle <= 90 && angle > 0)
         {
-            animator.SetFloat("MovementForward", movementAxis.y);
-            animator.SetFloat("MovementSides", movementAxis.x);
+            if (angle <= 90 && angle > 60)
+            {
+                animator.SetFloat("MovementForward", movementAxis.y);
+                animator.SetFloat("MovementSides", movementAxis.x);
+            }
+            else if (angle < 60 && angle > 30)
+            {
+                animator.SetFloat("MovementForward", movementAxis.y);
+                animator.SetFloat("MovementSides", movementAxis.x);
+            }
+            else if (angle < 30 && angle >= 0)
+            {
+                animator.SetFloat("MovementForward", movementAxis.x);
+                animator.SetFloat("MovementSides", movementAxis.y);
+            }
         }
 
-        if (angle > -180 && angle < -90)
+        if (angle >= -180 && angle < -90)
         {
-            animator.SetFloat("MovementForward", -movementAxis.y);
-            animator.SetFloat("MovementSides", -movementAxis.x);
+            if (angle >= -180 && angle < -150)
+            {
+                animator.SetFloat("MovementForward", -movementAxis.x);
+                animator.SetFloat("MovementSides", -movementAxis.y);
+            }
+            else if (angle > -150 && angle < -120)
+            {
+                animator.SetFloat("MovementForward", -movementAxis.y);
+                animator.SetFloat("MovementSides", -movementAxis.x);
+            }
+            else if (angle > -120 && angle <= -90)
+            {
+                animator.SetFloat("MovementForward", -movementAxis.y);
+                animator.SetFloat("MovementSides", -movementAxis.x);
+            }
         }
 
-        if (angle > -90 && angle < 0)
+        if (angle >= -90 && angle < 0)
         {
-            animator.SetFloat("MovementForward", -movementAxis.y);
-            animator.SetFloat("MovementSides", -movementAxis.x);
+            if (angle >= -90 && angle < -60)
+            {
+                animator.SetFloat("MovementForward", -movementAxis.y);
+                animator.SetFloat("MovementSides", -movementAxis.x);
+            }
+            else if (angle > -60 && angle < -30)
+            {
+                animator.SetFloat("MovementForward", -movementAxis.y);
+                animator.SetFloat("MovementSides", -movementAxis.x);
+            }
+            else if (angle > -30 && angle <= 0)
+            {
+                animator.SetFloat("MovementForward", movementAxis.x);
+                animator.SetFloat("MovementSides", -movementAxis.y);
+            }
         }
     }
 
     private void Attack()
     {
         // Attacks if the player currently is in the idle state (else it queues and starts once arrived)
-        if (Input.GetButtonDown("Fire1") && animator.GetCurrentAnimatorStateInfo(0).IsName("Armature_Idle_Sword_And_Shield"))
+        if (Input.GetButtonDown("Fire1") && animator.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
         {
             animator.SetTrigger("AttackTrigger");
         }
