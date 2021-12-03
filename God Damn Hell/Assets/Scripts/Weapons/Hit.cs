3ffd0c630591e39fd88747aa59f54962c521a660
Stjpaa <87935031+Stjpaa@users.noreply.github.com>
2021-12-03 15:51:24 +0100

    Fixed Animations while running + damage only when attacking

--- God Damn Hell/Assets/Scripts/Weapons/Hit.cs
+++ God Damn Hell/Assets/Scripts/Weapons/Hit.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class Hit : MonoBehaviour
 {
+    public Animator animator;
+
     public virtual void OnCollisionEnter(Collision other)
     {
-        if (other.gameObject.transform.parent.tag == "Enemy")
+        if (other.gameObject.transform.parent.tag == "Enemy" && animator.GetCurrentAnimatorStateInfo(1).IsName("Attack"))
         {
             // <EnemyStats> should just be stats once actual enemies exist
             other.gameObject.transform.parent.GetComponent<EnemyStats>().healthPoints--;
             Debug.Log("Hit the enemy!");
+            
         }
     }
 }
