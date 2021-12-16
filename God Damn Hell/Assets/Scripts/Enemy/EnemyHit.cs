8502a47d04f9738f57390455dcbdf567c991fcf9
Stjpaa <87935031+Stjpaa@users.noreply.github.com>
2021-12-16 23:09:12 +0100

    Added more enemy animations and reworked death animation system of player

--- God Damn Hell/Assets/Scripts/Enemy/EnemyHit.cs
+++ God Damn Hell/Assets/Scripts/Enemy/EnemyHit.cs
@@ -14,6 +14,8 @@ public class EnemyHit : MonoBehaviour
 
     public virtual void OnTriggerEnter(Collider other)
     {
+        // Colliders are on the enemies hands, code below only works if the target hit is the player
+        // lastHit acts as a cooldown so the player cant get hit multiple times within one animation
         if (lastHit >= 1f && other.gameObject.transform.tag == "Player" && (animator.GetCurrentAnimatorStateInfo(1).IsName("AttackLeft") || animator.GetCurrentAnimatorStateInfo(1).IsName("AttackRight")))
         {
             other.gameObject.GetComponent<PlayerStats>().healthPoints--;
