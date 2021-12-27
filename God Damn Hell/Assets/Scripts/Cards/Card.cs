2ca486bf1ca8f7b48dab4d42d91af2a263aeb757
MarhalmGH <93052627+MarhalmGH@users.noreply.github.com>
2021-12-27 02:15:15 +0100

    Card as Scriptable, first try

---
+++ God Damn Hell/Assets/Scripts/Cards/Card.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="New Card", menuName = "Card") ]
+public class Card : ScriptableObject
+{
+    public Sprite cardsprite;   // also divideable into artwork, description, basic card border
+    public Mesh equipment;
+
+    public int defense;     //optional
+    public int cost;
+}
+    
