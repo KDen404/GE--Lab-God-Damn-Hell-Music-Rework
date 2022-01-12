192e2f568c7047325949b558359c9cf8fabbc672
MarhalmGH <93052627+MarhalmGH@users.noreply.github.com>
2022-01-12 13:34:45 +0100

    Safgame to json

---
+++ God Damn Hell/Assets/Scripts/Cards/Safegame.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+[System.Serializable]
+public class Safegame{ 
+    public string[] equipmentSafedata = { "emptyString", "emptyString", "emptyString" };
+    
+    //private int armor;      
+    //private int potionAmount;
+}
+
