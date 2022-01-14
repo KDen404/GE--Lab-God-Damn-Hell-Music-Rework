197771b0ab837635a7224b5018f3ebd37c56d4af
Dennis Köhler <40216282+KDen404@users.noreply.github.com>
2022-01-14 17:07:11 +0100

    Added SoundBanks
    Attached sound banks to prefabs, added sound banks, added sounds :)

--- God Damn Hell/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ God Damn Hell/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -11,6 +11,38 @@
 
 namespace AK
 {
+    namespace EVENTS
+    {
+        static const AkUniqueID ATTACKBLOCKED = 1654192851U;
+        static const AkUniqueID DEMONFIGHTERDEATH = 725206385U;
+        static const AkUniqueID DEMONFIGHTERGOTHIT = 3929558812U;
+        static const AkUniqueID DEMONFIGHTERRUN = 36191336U;
+        static const AkUniqueID DEMONFIGHTERWALK = 3496783742U;
+        static const AkUniqueID DEMONMAGEGOTHIT = 1895283565U;
+        static const AkUniqueID DEMONMAGERUN = 4294451055U;
+        static const AkUniqueID DEMONMAGEWALK = 2140539667U;
+        static const AkUniqueID DEMONSPEARDEATH = 2102040365U;
+        static const AkUniqueID DEMONSPEARGOTHIT = 4061880024U;
+        static const AkUniqueID DEMONSPEARRUN = 537156876U;
+        static const AkUniqueID DEMONSPEARWALK = 3167257242U;
+        static const AkUniqueID FIGHTERSWING = 2867028574U;
+        static const AkUniqueID INGAMEMUSIC = 1545821079U;
+        static const AkUniqueID MAIN_MENU = 2005704188U;
+        static const AkUniqueID PLAYERDEATH = 1656947812U;
+        static const AkUniqueID PLAYERGOTHIT = 855695183U;
+        static const AkUniqueID PLAYERRUN = 3964335501U;
+        static const AkUniqueID SPEARSWING = 478914142U;
+        static const AkUniqueID SWORDSWING = 1328934038U;
+    } // namespace EVENTS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID ENEMIES = 2242381963U;
+        static const AkUniqueID MAIN_MENU = 2005704188U;
+        static const AkUniqueID PLAYER = 1069431850U;
+    } // namespace BANKS
+
     namespace BUSSES
     {
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
