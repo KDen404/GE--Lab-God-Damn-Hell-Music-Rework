73e20d597708a6c9d03038f4c9a15a52593c0aa4
MarhalmGH <93052627+MarhalmGH@users.noreply.github.com>
2022-01-18 00:01:45 +0100

    added pause menu

---
+++ God Damn Hell/Assets/Scripts/UI Interactions/PauseScreen.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseScreen : MonoBehaviour
+{
+    public GameObject pauseMenuBackground;
+    private void Start()
+    {
+        pauseMenuBackground.SetActive(false);
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            togglePauseScreen();
+        }
+    }
+
+    public void togglePauseScreen()
+    {
+        pauseMenuBackground.SetActive(!pauseMenuBackground.activeSelf);
+        if (pauseMenuBackground.activeSelf)     // un- /pauses the game based on pauseMenuBackground.activeSelf, if (Time.timeScale == 1.0) can cause errors based on float comparisons
+        {
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            Time.timeScale = 1.0f;
+        }
+    }
+}
